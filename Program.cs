1c478cb009e4a0fdcac42eac104df06bcd5b1440
VarunPabba <86397796+VarunPabba@users.noreply.github.com>
2021-06-24 16:19:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+       public string model;
+       public string color;
+       public int year;
+
+       public Program(string modelName, string modelColor, int modelYear)
+           {
+               model = modelName;
+               color = modelColor;
+               year = modelYear;
+           }
+
+       static void Main(string[] args)
+           {
+               Program Ford = new Program("Mustang", "Red", 1969);
+               Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+           }
+}
